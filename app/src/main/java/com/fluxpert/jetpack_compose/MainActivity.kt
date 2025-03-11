28b37b2267f354f7c3c9ad4fa9b41a2f3e78323a
Atif Waheed <a.waheed@globalfinancialmedia.com>
2025-03-11 10:28:17 +0500

    issues fixed.

--- app/src/main/java/com/fluxpert/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/fluxpert/jetpack_compose/MainActivity.kt
@@ -5,24 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,16 +45,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeArticle(
-    image: Painter,
-    title: String,
-    paragraphs: List<String>,
-    modifier: Modifier = Modifier
+    image: Painter, title: String, paragraphs: List<String>, modifier: Modifier = Modifier
 ) {
     val textModifier: Modifier = Modifier.padding(
-        start = 16.dp,
-        top = 10.dp,
-        end = 16.dp,
-    );
+        start = 16.dp, top = 10.dp, end = 16.dp
+    )
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Top,
