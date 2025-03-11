f0222f5bfffe478cb78d47d434937c49fc75b2aa
Atif Waheed <a.waheed@globalfinancialmedia.com>
2025-03-11 09:47:49 +0500

    Jetpack Compose - Happy Birthday 20%

---
+++ README.md
@@ -0,0 +1,1 @@
+# [Tutorial Link](https://developer.android.com/courses?gad_source=1&gclid=Cj0KCQjwm7q-BhDRARIsACD6-fXRyqwabFw7DOrmkaucLk8N6PGcYYhY58mPLTloAAF1LkG9pC0gXJkaAh2TEALw_wcB&gclsrc=aw.ds)
---
+++ app/src/main/java/com/fluxpert/jetpack_compose/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.fluxpert.jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fluxpert.jetpack_compose.ui.theme.Jetpack_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_composeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Greeting(
+                        message = "Android", from = "Atif"
+                    )
+                }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+        )
+        GreetingText(
+            message = message, from = from, modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = stringResource(R.string.happy_birthday_string, message),
+            fontSize = 80.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "from $from",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Jetpack_composeTheme {
+        Greeting("Atif", "Android")
+    }
+}
+
+
+/// Just programming code for learning.
+//fun main() {
+//    val count: Int = 2
+//    println("Value is: $count")
+//    var nullable: Int?= null
+//    var nullable2: Int?= null
+//    println("nullable is $nullable")
+//    nullable = nullable2 ?: 201
+//    println("nullable updated to $nullable")
+//    println(myFunction("Atif", nullable))
+//}
+//
+//fun myFunction(name: String, age: Int = 20): String?{
+//    if(age > 100){
+//        return null
+//    }
+//    var ageS: String = "s"
+//    if(age < 2 && age > -1){
+//        ageS = ""
+//    } else{
+//        ageS = "(s)"
+//    }
+//    return "Hello $name, you're $age year$ageS old";
+//}
